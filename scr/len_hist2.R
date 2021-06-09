a05d93ef0e46d79a06283e9d882f5963ee935c00
bbogdanovaalina <bbogdanovaalina@gmail.com>
2021-06-10 02:27:05 +0300

    еще гистограмма

---
+++ scr/len_hist2.R
@@ -0,0 +1,73 @@
+source('lib.R')
+
+NAME <- 'DeepZ'
+#NAME <- 'H3K9me3_SK-N-SH.ENCFF501EUA.hg19'
+#NAME <- 'H3K9me3_SK-N-SH.ENCFF709THW.hg19'
+
+
+bed_df <- read.delim(paste0(DATA_DIR, NAME, '.bed'), as.is = TRUE, header = FALSE)
+#colnames(bed_df) <- c('chrom', 'start', 'end', 'name', 'score')
+colnames(bed_df) <- c('chrom', 'start', 'end')
+bed_df$len <- bed_df$end - bed_df$start
+head(bed_df)
+
+
+ggplot(bed_df) +
+  aes(x = len) +
+  geom_histogram() +
+  ggtitle(NAME, subtitle = sprintf('Number of peaks = %s', nrow(bed_df))) +
+  theme_bw()
+ggsave(paste0('filter_peaks.', NAME, '.init.hist.pdf'), path = OUT_DIR)
+
+# Remove long peaks
+bed_df <- bed_df %>%
+  arrange(-len) %>%
+  filter(len < 5000)
+
+ggplot(bed_df) +
+  aes(x = len) +
+  geom_histogram() +
+  ggtitle(NAME, subtitle = sprintf('Number of peaks = %s', nrow(bed_df))) +
+  theme_bw()
+ggsave(paste0('filter_peaks.', NAME, '.filtered.hist.pdf'), path = OUT_DIR)
+
+bed_df %>%
+  select(-len) %>%
+  write.table(file=paste0('data/', NAME ,'.filtered.bed'),
+              col.names = FALSE, row.names = FALSE, sep = '\t', quote = FALSE)
+
+
+#теперь для второго файла 
+
+#NAME <- 'H3K9me3_SK-N-SH.ENCFF501EUA.hg19'
+NAME <- 'H3K9me3_SK-N-SH.ENCFF709THW.hg19'
+bed_df <- read.delim(paste0('data/', NAME, '.bed'), as.is = TRUE, header = FALSE)
+colnames(bed_df) <- c('chrom', 'start', 'end', 'name', 'score')
+bed_df$len <- bed_df$end - bed_df$start
+head(bed_df)
+
+ggplot(bed_df) +
+  aes(x = len) +
+  geom_histogram() +
+  ggtitle(NAME, subtitle = sprintf('Number of peaks = %s', nrow(bed_df))) +
+  theme_bw()
+ggsave(paste0('filter_peaks.', NAME, '.init.hist.pdf'), path = OUT_DIR)
+
+# Remove long peaks
+bed_df <- bed_df %>%
+  arrange(-len) %>%
+  filter(len < 3000)
+
+ggplot(bed_df) +
+  aes(x = len) +
+  geom_histogram() +
+  ggtitle(NAME, subtitle = sprintf('Number of peaks = %s', nrow(bed_df))) +
+  theme_bw()
+ggsave(paste0('filter_peaks.', NAME, '.filtered.hist.pdf'), path = OUT_DIR)
+
+bed_df %>%
+  select(-len) %>%
+  write.table(file=paste0('data/', NAME ,'.filtered.bed'),
+              col.names = FALSE, row.names = FALSE, sep = '\t', quote = FALSE)
+
+
