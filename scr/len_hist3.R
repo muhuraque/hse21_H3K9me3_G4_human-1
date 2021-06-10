d710f437d315836cd5b36e4bfe570a89e5c73752
bbogdanovaalina <bbogdanovaalina@gmail.com>
2021-06-10 03:40:46 +0300

    .

---
+++ scr/len_hist3.R
@@ -0,0 +1,19 @@
+source('lib.R')
+
+NAME <- 'H3K9me3_SK-N-SH.intersect_with_DeepZ'
+#NAME <- 'H3K9me3_SK-N-SH.ENCFF501EUA.hg19'
+#NAME <- 'H3K9me3_SK-N-SH.ENCFF709THW.hg19'
+
+
+bed_df <- read.delim(paste0(DATA_DIR, NAME, '.bed'), as.is = TRUE, header = FALSE)
+colnames(bed_df) <- c('chrom', 'start', 'end')
+bed_df$len <- bed_df$end - bed_df$start
+head(bed_df)
+
+
+ggplot(bed_df) +
+  aes(x = len) +
+  geom_histogram() +
+  ggtitle(NAME, subtitle = sprintf('Number of peaks = %s', nrow(bed_df))) +
+  theme_bw()
+ggsave(paste0('filter_peaks.', NAME, '.init.hist.pdf'), path = OUT_DIR)
